f642265da43136d32ecf40aa19de9a32129dad06
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-12-29 09:27:44 +0000

    Outputing staff list to check that command still works

--- commands/enforce-2fa.js
+++ commands/enforce-2fa.js
@@ -34,6 +34,7 @@ function cmd(bosco, args, next) {
             process.exit(1);
         }
         ghclient.getAll2faDisabledStaff(org, function(err, staffList) {
+            console.log('Found\n' + staffList);
             async.mapSeries(staffList, function(staff, cb) {
                 if(!team) {
                     console.log('Removing ' + staff + ' from ' + org);
